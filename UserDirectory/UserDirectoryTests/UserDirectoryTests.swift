8c54ed79ed736d0b6250000dd36fd37bc1b19b76
faysalf <mdfaysalahmedf@gmail.com>
2025-09-15 02:32:00 +0600

    test method written for failure cases

--- UserDirectory/UserDirectoryTests/UserDirectoryTests.swift
+++ UserDirectory/UserDirectoryTests/UserDirectoryTests.swift
@@ -27,7 +27,7 @@ final class UserDirectoryTests: XCTestCase {
         
     }
 
-    // MARK: - Test 1:- Login API call with mock response.
+    // MARK: - Test 1:- Login API call with mock response (for success)
     func testLoginSuccessPublishesTrue() {
         let mockService = MockAuthService()
         mockService.tokenToReturn = "token.faysal.123"
@@ -51,8 +51,31 @@ final class UserDirectoryTests: XCTestCase {
         
     }
     
+    // Test 1:- Login API call with mock response (for Failure)
+    func testLoginFailurePublishesError() {
+        let mockService = MockAuthService()
+        mockService.shouldFail = true
+        mockService.errorToReturn = URLError(.notConnectedToInternet)
+        
+        let vm = AuthenticationViewModel(service: mockService)
+        let expec = expectation(description: "Login failure")
+        
+        let cancellable = vm.$errorMessage
+            .dropFirst()
+            .sink { msg in
+                if msg != nil {
+                    XCTAssertNotNil(msg)
+                    expec.fulfill()
+                }
+            }
+        
+        vm.login(email: "faysalf@gmail.com", password: "fromDhaka")
+        wait(for: [expec], timeout: 3)
+        cancellable.cancel()
+        
+    }
     
-    // MARK: - Test 2:- Users list API parsing.
+    // MARK: - Test 2:- Users list API parsing (for success)
     func testFetchUsersFromReqRes() {
         let expectation = expectation(description: "Fetch real users from ReqRes")
         let vm = DashboardViewModel(service: DashboardService())
@@ -70,10 +93,34 @@ final class UserDirectoryTests: XCTestCase {
         vm.getUsers()
 
         wait(for: [expectation], timeout: 5)
+    }
+
+    // Test 2:- Users list API parsing (for failure)
+    func testFetchUsersFailureSetsError() {
+        let fakeResponse = UserListModel(page: 1, perPage: 1, total: 1, totalPages: 12, data: [])
+        let mockService = MockDashboardService(sampleResponse: fakeResponse)
+        mockService.shouldFail = true
+        mockService.errorToReturn = URLError(.notConnectedToInternet)
+        
+        let vm = DashboardViewModel(service: mockService)
+        let expectation = expectation(description: "Setting error for users list")
+
+        vm.$errorMessage
+            .dropFirst()
+            .sink { msg in
+                if msg != nil {
+                    XCTAssertNotNil(msg)
+                    expectation.fulfill()
+                }
+            }
+            .store(in: &cancellables)
+
+        vm.getUsers()
+        
+        wait(for: [expectation], timeout: 2)
         
     }
-    
-    
+
     // MARK: - Test3:- Token storage & retrieval.
     func testTokenStorageAndRetrieval() {
         let testToken = "ahmed.123"
@@ -86,18 +133,52 @@ final class UserDirectoryTests: XCTestCase {
         
     }
 
+    // Test3:- Token deletion
+    func testTokenDeletion() {
+        let testToken = "ahmed.123"
+        _ = kcm.saveToken(testToken)
+        let deleteSuccess = kcm.deleteToken()
+        XCTAssertTrue(deleteSuccess)
+        XCTAssertNil(kcm.getToken())
+    }
+    
 }
 
 // MARK: - Mock Auth Service class
 final class MockAuthService: AuthenticationServiceProtocol {
     var tokenToReturn = "fake.token.123"
+    var shouldFail = false
+    var errorToReturn: Error = URLError(.badServerResponse)
     
     func login(email: String, password: String) -> AnyPublisher<LoginResponseModel, Error> {
+        if shouldFail {
+            return Fail(error: errorToReturn).eraseToAnyPublisher()
+        }
         let response = LoginResponseModel(token: tokenToReturn)
         return Just(response)
             .setFailureType(to: Error.self)
             .eraseToAnyPublisher()
+    }
+    
+}
 
+// MARK: - Mock Dashboard Service Class
+final class MockDashboardService: DashboardServiceProtocol {
+    var sampleResponse: UserListModel
+    var shouldFail = false
+    var errorToReturn: Error = URLError(.badServerResponse)
+
+    init(sampleResponse: UserListModel) {
+        self.sampleResponse = sampleResponse
+    }
+
+    func getUserList(currentPage: Int, pageSize: Int) -> AnyPublisher<UserListModel, Error> {
+        if shouldFail {
+            return Fail(error: errorToReturn).eraseToAnyPublisher()
+        }
+        return Just(sampleResponse)
+            .setFailureType(to: Error.self)
+            .eraseToAnyPublisher()
     }
     
 }
